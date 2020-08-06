ded22fc0a2c5e4a150aa19515f7a0cbc5aaf9cfc
wanggang18 <wanggang18@longfor.com>
2020-08-06 23:20:23 +0800

    在read中写了一句话

--- src/main/java/mq/MqLearn.java
+++ src/main/java/mq/MqLearn.java
@@ -8,5 +8,6 @@ public class MqLearn {
     public static void main(String[] args) {
         System.out.println("这是一个rabbit类");
         System.out.println("在master写了这句话");
+        System.out.println("在read中写了这句话");
     }
 }
